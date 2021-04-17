bbbdb0f516adc3d867cf119b133236a5b57cdd0e
Nagesh Raj <nagesh@wolkensoftware.com>
2021-04-17 19:40:25 +0530

    CRUD operations completed

--- src/main/java/com/wolkensoftware/AadharSpringBoot/AadharSpringBootApplication.java
+++ src/main/java/com/wolkensoftware/AadharSpringBoot/AadharSpringBootApplication.java
@@ -8,10 +8,6 @@ public class AadharSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AadharSpringBootApplication.class, args);
-
-//		System.out.println("\nHey Divya!");
-//		System.out.println("9.2/10");
-//		System.out.println("This is way better than the previous pic xD");
 	}
 
 }
